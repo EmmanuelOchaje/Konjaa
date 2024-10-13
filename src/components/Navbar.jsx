16dabe97eaf430b25044368c4ec288a2af7a2371
EmmanuelOchaje <114095241+EmmanuelOchaje@users.noreply.github.com>
2024-10-13 20:53:45 +0100

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,23 +24,23 @@ const Navbar = () => {
           menu ? "top-[0]" : "top-[-100%]"
         } duration-300 w-screen h-fit shadow-lg`}
       >
-        <span className="float-right mr-8 mt-10 text-5xl text-black">
+        <span className="float-right mr-8 mt-8 md:mt-10 md:text-5xl text-3xl text-black">
           <i class="bx bx-x" onClick={() => showMenu()}></i>
         </span>
         <ul className="mx-4 mt-12 my-">
-          <li className="text-[#777777] text-[18px] mb-8 mr-8 font-normal">
+          <li className="text-[#777777] text-[14px] md:text-[18px] mb-8 mr-8 font-normal">
             <Link to="/">Home</Link>
           </li>
-          <li className="text-[#777777] text-[18px] mb-8 mr-8 font-normal">
+          <li className="text-[#777777] text-[14px] md:text-[18px] mb-8 mr-8 font-normal">
             <Link to="/about">About</Link>
           </li>
-          <li className="text-[#777777] text-[18px] mb-8 mr-8 font-normal">
+          <li className="text-[#777777] text-[14px] md:text-[18px] mb-8 mr-8 font-normal">
             <Link to="/guide">Guide</Link>
           </li>
-          <li className="text-[#777777] text-[18px] mb-8 mr-8 font-normal">
+          <li className="text-[#777777] text-[14px] md:text-[18px] mb-8 mr-8 font-normal">
             <Link to="/privacy">Privacy</Link>
           </li>
-          <li className="text-[#777777] text-[18px] mb-8 font-normal">
+          <li className="text-[#777777] text-[14px] md:text-[18px] mb-8 font-normal">
             Partners
           </li>
           <li className="text-black mb-5  font-semibold">
@@ -52,26 +52,30 @@ const Navbar = () => {
       </div>
 
       <div className="flex justify-between lg:pt-8 py-4 px-2 lg:px-[6rem] items-center">
-        <img src={logo} className="w-50 cursor-pointer h-12" alt="Pontify" />
+        <img
+          src={logo}
+          className="w-[150px] h-[40px] mt-4 md:mt-0 md:w-50 cursor-pointer md:h-12"
+          alt="Pontify"
+        />
 
         <div>
           <ul className="lg:flex hidden mr-12">
-            <li className="cursor-pointer mr-8 font-semibold">
+            <li className=" cursor-pointer mr-8 font-semibold">
               <Link to="/">Home</Link>
             </li>
-            <li className="cursor-pointer mr-8 font-semibold">
+            <li className=" cursor-pointer mr-8 font-semibold">
               <Link to="/about">About</Link>
             </li>
 
-            <li className="cursor-pointer mr-8 font-semibold">
+            <li className=" cursor-pointer mr-8 font-semibold">
               <Link to="/guide">Guide</Link>
             </li>
-            <li className="cursor-pointer mr-8 font-semibold">
+            <li className=" cursor-pointer mr-8 font-semibold">
               <Link to="/privacy">Privacy</Link>
             </li>
-            <li className="cursor-pointer mr- font-semibold">Partners</li>
+            <li className=" cursor-pointer mr- font-semibold">Partners</li>
           </ul>
-          <span className="float-right lg:hidden mr-6 mt-6 text-5xl text-black">
+          <span className="float-right lg:hidden mr-6 mt-4 md:mt-6 text-3xl md:text-5xl text-black">
             <i class="bx bx-menu" onClick={() => showMenu()}></i>
           </span>
         </div>
